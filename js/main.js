688e62b971ee3f8ed2aaab23a4b8d5449584ebf6
bcl1713 <bcl1713@gmail.com>
2021-06-21 21:49:38 +0200

    test

--- js/main.js
+++ js/main.js
@@ -9,11 +9,9 @@
  * Date        Comments
  */
 
-import * as THREE from './libs/three.module.js';
-
-
 function init() {
-  const scene = new THREE.Scene();
+  const scene = Scene
+  const cam = new THREE.
   const camera = new THREE.PerspectiveCamera (75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer( {
     antialias: true
