748a3ff839a3b8879e5d8f79eeea6cf332c4b4e5
Brian Lucas <bcl1713@gmail.com>
2021-06-21 22:06:39 +0200

    test

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@
  * Date        Comments
  */
 
+import * as THREE from './lib/three.module.js';
+
+
 function init() {
-  const scene = Scene
-  const cam = new THREE.
+  const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera (75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer( {
     antialias: true
