52ceef9a1da07578091ae52925be95fde712d10c
Brian Lucas <bcl1713@gmail.com>
2021-06-22 09:35:56 +0200

    randomize the Z

--- js/main.js
+++ js/main.js
@@ -15,21 +15,28 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 
-console.log(scene);
-console.log(camera);
-console.log(renderer);
-
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000, 
-  side: THREE.DoubleSide
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
 scene.add(planeMesh);
+
+console.log(planeMesh.geometry.attributes.position.array);
+
+const {array} = planeMesh.geometry.attributes.position
+
+for (let i = 0; i < array.length; i+= 3) {
+ 
+  array[i + 2] = array[i + 2] + Math.random()
+
+}
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 5);
