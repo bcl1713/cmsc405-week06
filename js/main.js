42a4eb511277d74dac647a98377f54603755239b
Brian Lucas <bcl1713@gmail.com>
2021-06-22 09:05:16 +0200

    add a box and spin it

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ import * as THREE from './lib/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
 console.log(scene);
 console.log(camera);
@@ -21,3 +21,19 @@ console.log(renderer);
 
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+}
+
+animate();
