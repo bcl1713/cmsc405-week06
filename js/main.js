d801f4726981cda1bcaf9063069f1c4b667d2c8c
Brian Lucas <bcl1713@gmail.com>
2021-06-22 09:15:55 +0200

    add plane

--- js/main.js
+++ js/main.js
@@ -23,10 +23,19 @@ renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const mesh = new THREE.Mesh(boxGeometry, material);
 
 scene.add(mesh);
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff0000, 
+  side: THREE.DoubleSide
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(planeMesh);
 
 camera.position.z = 5;
 
@@ -36,6 +45,7 @@ function animate() {
   mesh.rotation.x += 0.02;
   mesh.rotation.y += 0.03;
   mesh.rotation.z += 0.05;
+  planeMesh.rotation.x += 0.01;
 }
 
 animate();
