512c42575d8873d17e0746a2617676e61a5eff36
Brian Lucas <bcl1713@gmail.com>
2021-06-22 08:53:28 +0200

    Fix renderer initializer (by removing it for now)

--- js/main.js
+++ js/main.js
@@ -13,9 +13,7 @@ import * as THREE from './lib/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer( {
-  antialias=true
-} );
+const renderer = new THREE.WebGLRenderer();
 
 console.log(scene);
 console.log(camera);
