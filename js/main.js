7518b4f2a3bb22c6809511f7b71a66b54d7b2c05
Brian Lucas <bcl1713@gmail.com>
2021-06-27 12:05:02 +0200

    refix sat size

--- js/main.js
+++ js/main.js
@@ -90,21 +90,21 @@ const earthMesh = new THREE.Mesh(
   })
 );
 
-let sateliteOrbit = [];
+// let sateliteOrbit = [];
 
-const sateliteMesh = new THREE.Mesh(
-  new THREE.CylinderGeometry(1.5, 1.5, 3.1, 100, 100),
-  new THREE.MeshPhongMaterial({
-    color: 0x777777,
-    shininess: 128
-  })
-)
+// const sateliteMesh = new THREE.Mesh(
+//   new THREE.CylinderGeometry(1.5, 1.5, 3.1, 100, 100),
+//   new THREE.MeshPhongMaterial({
+//     color: 0x777777,
+//     shininess: 128
+//   })
+// )
 
 const sateliteGroup = new THREE.Group();
 const satelites = new Array(11);
 for (let i = 0; i < 11; i++) {
   satelites[i] = new THREE.Mesh(
-    new THREE.CylinderGeometry(.015, .015, .031, 100, 100),
+    new THREE.CylinderGeometry(.15, .15, .31, 100, 100),
     new THREE.MeshPhongMaterial({
       color: 0x777777,
       shininess: 128
